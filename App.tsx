15e72b7cd049477f4867b72cedb68b4511625591
unknown <lcapelotto@gmail.com>
2022-06-20 16:29:33 -0300

    1.0

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import DrawlerNavigation from './src/routes/drawlerNavigation/DrawlerNavigation';
 import StackNavigation from './src/routes/stackNavigation/StackNavigation';
 import TabNavigation from './src/routes/tabNavigation/TabNavigation';
-
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
